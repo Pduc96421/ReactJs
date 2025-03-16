d7375b01a619dd3a94c139b6fe7882ff2405b00c
Pduc96421 <pduc96421@gmail.com>
2025-03-16 20:19:34 +0700

    UseStatus

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,10 +1,65 @@
+import { memo } from "react";
+import { useState } from "react";
+import Content from "./Content";
 
-function App() {
+const courses = [
+  {
+    id: 1,
+    name: 'html, css'
+  },
+  {
+    id: 2,
+    name: 'js'
+  },
+  {
+    id: 3,
+    name: 'reactJs'
+  }
+]
+
+const App = () => {
+  const [checked, setChecked] = useState([]);
+
+  const handleCheck = (id) => {
+    setChecked(prev => {
+      const isChecked = checked.includes(id);
+      if(isChecked){
+        return checked.filter(item => item !== id);
+      } else {
+        return [...prev, id];
+      }
+    })
+  }
+
+  const handleSubmit = () => {
+    // call api
+    console.log({ id: checked });
+  }
+
+  const [show, setShow] = useState(false);
+
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div style={{ padding: 32 }}>    
+      <div>
+        {courses.map(course => (
+          <div key={course.id}>
+            <input 
+              type="checkbox" 
+              checked={checked.includes(course.id)}
+              onChange={() => handleCheck(course.id)}
+            /> {course.name}
+          </div>
+        ))}
+      </div>
+
+      <button onClick={handleSubmit}>Register</button>
+
+      <div style={{ padding: 32}}>
+        <button onClick={() => setShow(!show)}>Toggle</button>
+        {show && <Content />}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ fe/src/Content.js
@@ -0,0 +1,8 @@
+
+const  Content = () =>  {
+  return (
+    <h1>hi anh em</h1>
+  )
+}
+
+export default Content;
